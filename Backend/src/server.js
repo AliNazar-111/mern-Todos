e497a92d1b7e5398a32fa835b6f3f66c569da9f7
AliNazar-111 <muhammadali2005111@gmail.com>
2025-08-09 19:25:39 +0500

    second commit

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import path from "path";
 import NotesRoutes from "./routes/NotesRoutes.js";
 import { connectDB } from "./config/db.js";
 import ratelimiter from "./middleware/rateLimiter.js";
@@ -9,6 +9,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve(__dirname, "../Frontend/dist");
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(ratelimiter);
 
 app.use("/api/notes", NotesRoutes);
+app.use(express.static(path.join));
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("server started");

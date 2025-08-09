a536e0733965e652e5ce5a3b8df8a615d17ad2dd
AliNazar-111 <muhammadali2005111@gmail.com>
2025-08-10 03:08:14 +0500

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router"
+import { Route, Routes } from "react-router-dom"
 import HomePage from "./Pages/HomePage"
 import CreatePage from "./Pages/CreatePage"
 import NoteDetailPage from "./Pages/NoteDetailPage"

6896e909354c6cb3fafe13cf6b813dc456e8734f
AliNazar-111 <muhammadali2005111@gmail.com>
2025-09-27 18:31:10 +0500

    K

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -4,7 +4,7 @@ export const connectDB = async () => {
   // <--- Ensure 'export' is here
   try {
     // Ensure process.env.Mongo_URI is defined
-    if (!process.env.Mongo_URI) {
+    if (!process.env.MONGO_URI) {
       throw new Error("Mongo_URI environment variable is not defined.");
     }
     await mongoose.connect(process.env.Mongo_URI);
